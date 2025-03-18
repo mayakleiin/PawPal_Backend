e4318476d67a0faa9abbb8cf0946f00ffeaa8275
mayakleiin <mayaklein1999@cs.colman.ac.il>
2025-03-18 02:59:09 +0200

    auth, jwt

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,47 +2,52 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 import path from "path";
-import cors from "cors"; // Enable CORS for cross-origin requests
-import postRoutes from "./routes/post_route"; // Import post routes
+import authRoutes from "./routes/auth_route";
 
+import postRoutes from "./routes/post_route"; // Import post routes
 dotenv.config();
 const app = express();
 
-const initApp = (): Promise<Express> => {
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/posts", postRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/auth", authRoutes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Web Dev 2025 REST API",
+      version: "1.0.0",
+      description: "REST server including authentication using JWT",
+    },
+    servers: [{ url: "http://localhost:3000" }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+const initApp = async () => {
   return new Promise<Express>((resolve, reject) => {
     const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error:"));
-    db.once("open", function () {
-      console.log("Connected to the database");
+    db.on("error", (error) => {
+      console.error(error);
     });
-
+    db.once("open", function () {
+      console.log("Connected to Mongoose");
+    });
     if (!process.env.DB_CONNECT) {
-      reject("DB_CONNECT is not defined");
+      reject("No DB_CONNECT");
     } else {
-      mongoose
-        .connect(process.env.DB_CONNECT)
-        .then(() => {
-          app.use(bodyParser.json());
-          app.use(bodyParser.urlencoded({ extended: true }));
-          app.use(cors()); //Enables CORS for all routes
-
-          //Register API routes
-          app.use("/posts", postRoutes); // Connect the post routes
-          app.use(
-            "/uploads",
-            express.static(path.join(__dirname, "../uploads"))
-          );
-
-          app.get("/", (_req, res) => {
-            res.send("PawPal API is running");
-          });
-
-          resolve(app);
-        })
-        .catch((err: unknown) => {
-          reject(err);
-        });
+      mongoose.connect(process.env.DB_CONNECT).then(() => {
+        resolve(app);
+      });
     }
   });
 };
